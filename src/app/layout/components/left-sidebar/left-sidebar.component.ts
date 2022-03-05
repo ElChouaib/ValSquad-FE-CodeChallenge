d96ca927ea28e040c9a5c2a900fddd81c3aa1061
El-chouaib <chouaib.elmakhlouk@atoz-digital.lu>
2022-03-05 20:31:00 +0100

    sidebar implementing

--- src/app/layout/components/left-sidebar/left-sidebar.component.ts
+++ src/app/layout/components/left-sidebar/left-sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-left-sidebar',
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./left-sidebar.component.scss']
 })
 export class LeftSidebarComponent implements OnInit {
+  navigationButtons: any = [];
+  selectedButton: string = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
   }
 
+  ngOnInit(): void {
+    this.initNavigationButtons();
+    this.selectedButton = this.navigationButtons[0].name;
+  }
+
+  private initNavigationButtons() {
+    this.navigationButtons = [
+      {name: "Home", icon: "fa fa-home"},
+      {name: "Messages", icon: "fa fa-comment"},
+      {name: "Profile", icon: "fa fa-user"},
+      {name: "Saved Post", icon: "fa fa-bookmark"},
+      {name: "Settings", icon: "fa fa-cog"},
+
+    ]
+  }
+
+  updateNavigationButton(item: any) {
+    this.selectedButton = item.name;
+  }
 }
